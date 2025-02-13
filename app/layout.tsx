e07bc29c3a9ca19604a7b5a65e757b473110c872
ochudi <chukwudi.ofoma@pau.edu.ng>
2025-02-13 12:26:48 +0100

    Fix unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
