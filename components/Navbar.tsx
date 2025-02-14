6d62681c7ddc05c25731190a85166a520d218ea5
ochudi <chukwudi.ofoma@pau.edu.ng>
2025-02-14 23:51:06 +0100

    Fix Navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,11 @@ import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
-    <nav className="flex max-w-[1200px] h-[76px] px-4 justify-between items-center rounded-[24px] border border-[#197686] bg-[rgba(5,37,44,0.4)] backdrop-blur-[2px] mx-5 mt-[24px]">
+    <nav className="flex max-w-[1200px] h-[76px] px-4 justify-between items-center rounded-[24px] border border-[#197686] bg-[rgba(5,37,44,0.4)] backdrop-blur-[2px] mx-5 md:mx-auto mt-[24px]">
       <div className="flex items-center gap-2">
+        <Link 
+        href="/"
+        className="hover:text-gray-300 transition flex p-2.5 justify-center items-center gap-2.5 text-white text-[18px] font-normal leading-none font-jeju">
         <div className="flex p-[6px_8px] justify-center items-center gap-[10px] rounded-[12px] border border-[#0E464F] bg-[#052F35]">
           <Image src="/icons/logo.svg" width={24} height={24} alt="Ticz Icon" />
         </div>
@@ -16,17 +19,18 @@ const Navbar = () => {
           alt="Ticz"
           className="sm:block"
         />
+        </Link>
       </div>
 
       <div className="hidden sm:flex items-center gap-4">
         <Link
-          href="/events"
+          href="/"
           className="hover:text-gray-300 transition flex p-2.5 justify-center items-center gap-2.5 text-white text-[18px] font-normal leading-none font-jeju"
         >
           Events
         </Link>
         <Link
-          href="/my-tickets"
+          href="/"
           className="hover:text-gray-300 transition flex p-2.5 justify-center items-center gap-2.5 text-white text-[18px] font-normal leading-none font-jeju"
         >
           My Tickets
